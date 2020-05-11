ae8b56a47e0ade7d23cae55b37c344205b01d2f6
Тарас <taras06031@gmail.com>
2020-05-11 23:31:37 +0300

    backend works cors

--- functions/index.js
+++ functions/index.js
@@ -7,12 +7,6 @@ const FBAuth = require('./Utility/fbAuth');
 const cors = require('cors');
 app.use(cors());
 
-app.use(function(req,res, next){
-    res.header("Access-Control-Allow_Origin","*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type,Accept");
-    next();
-});
-
 
 const { db } = require('./Utility/admin');
 
